4bf0e9bb5598c919db9635b350d1d48ee2192413
ncRakl <nathan.raklure@gmail.com>
2020-12-28 18:39:19 +0100

    commented featureselection

--- featureselection.py
+++ featureselection.py
@@ -21,8 +21,7 @@ X = data.iloc[:,0:199]
 y = data.iloc[:,199]
 print(data.head())
 
-#%% Backward Elimination
-
+#%% 1. Backward Elimination
 cols = list(X.columns)
 pmax = 1
 while (len(cols) > 0):
@@ -41,14 +40,13 @@ while (len(cols) > 0):
 selected_features_BE = cols
 print(selected_features_BE)
 
-# Result :
+# Columns to keep by section 1 : 
 # ['V14', 'V84', 'V96', 'V159', 'V160', 'V161', 'V162', 'V163', 'V164', 'V165',
 # 'V166', 'V167', 'V168', 'V169', 'V170', 'V171', 'V172', 'V173', 'V174', 'V175',
 # 'V176', 'V177', 'V180', 'V181', 'V182', 'V183', 'V184', 'V185', 'V186', 'V187',
 # 'V188', 'V189', 'V190', 'V191', 'V192', 'V193', 'V194', 'V195', 'V197', 'V198', 'V199']
 
-#%% Removing constant, duplicates, correlated features
-
+#%% 2.1 Removing constant features
 columns_to_remove = set()
 
 constant_filter = VarianceThreshold(threshold=0.01)
@@ -59,6 +57,7 @@ columns_to_remove.update(constant_columns)
 X = X.drop(columns=constant_columns)
 print(X.shape)
     
+#%% 2.2 Removing duplicated features
 X_d = X.T.duplicated()
 duplicated_columns = list(X_d[X_d==True].index)
 
@@ -66,6 +65,7 @@ columns_to_remove.update(duplicated_columns)
 X = X.drop(columns=duplicated_columns)
 print(X.shape)
 
+#%% 2.3 Removing correlated features
 num_columns = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
 numerical_columns = list(X.select_dtypes(include=num_columns).columns)
 X_numonly = X[numerical_columns]
@@ -81,13 +81,14 @@ for i in range(len(correlation_matrix.columns)):
 columns_to_remove.update(correlated_features)
 X = X.drop(columns=correlated_features)
 
-#V194,V161,V173,V191,V198,V196,V195,V174,V178,V182,V197,V199
-#V186,V179,V169,V167,V165,V188,V192,V183,V176,V172,V177,V187
-#V184,V181,V175,V180
+# Columns to remove by section 2 :
+# V194,V161,V173,V191,V198,V196,V195,V174,V178,V182,V197,V199
+# V186,V179,V169,V167,V165,V188,V192,V183,V176,V172,V177,V187
+# V184,V181,V175,V180
 
-#%% SelectKBest
+#%% 3. SelectKBest
 
-bestfeatures = SelectKBest(score_func=f_classif, k=100)
+bestfeatures = SelectKBest(score_func=f_classif, k=100) # We set a custom k value, so here we want 100 features
 
 fit = bestfeatures.fit(X, y)
 
@@ -96,11 +97,11 @@ dfcolumns = pd.DataFrame(X.columns)
 
 featureScores = pd.concat([dfcolumns, dfscores], axis=1)
 featureScores.columns = ['Specs', 'Score']
-featureScores = featureScores[featureScores['Score']>0.05]
+featureScores = featureScores[featureScores['Score']>0.05] # We only take values greater than 5%
 #print(featureScores.nlargest(140, 'Score'))
 print(featureScores)
 
-#%% ExtraTreesClassifier
+#%% 4. ExtraTreesClassifier
 
 model = ExtraTreesClassifier()
 model.fit(X, y)
